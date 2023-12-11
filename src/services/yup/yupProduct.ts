efe99c8033efa39fdbc8b89b18a96ba10da22018
Brumadu <madureira12.bm@gmail.com>
2023-12-11 03:10:38 -0100

    feat: add product verification

---
+++ src/services/yup/yupProduct.ts
@@ -0,0 +1,8 @@
+import * as yup from 'yup';
+
+export const YupProductSchema = yup.object({
+  name: yup.string().required('Campo necessario: Produto'),
+  price: yup.number(),
+  description: yup.string().required('Campo necessario: Descrição'),
+  productPhoto: yup.string().required('Campo necessario: Foto'),
+});
